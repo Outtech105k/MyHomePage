88371817df2edd4ce656a75aab4849f735d1cb26
Outteck105k <techno510tk@gmail.com>
2023-11-22 10:36:29 +0900

    何もしていないはず

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-SAWAYAKA_DB_PATH='/home/outtech105/app/databases/sawayaka.db'
-BLOG_DB_PATH='/home/outtech105/app/databases/blog.db'
+SAWAYAKA_DB_PATH='databases/sawayaka.db'
+BLOG_DB_PATH='databases/blog.db'
