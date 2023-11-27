102147801e55e186896d0e6ed8cf8bcf4284ae06
Outteck105k <techno510tk@gmail.com>
2023-11-27 13:01:32 +0900

    各ファイルのpep8対応2

--- selenuim_test.py
+++ selenuim_test.py
@@ -4,37 +4,37 @@
 # https://self-development.info/ubuntuでseleniumを動かしてスクレイピングを行う【python】/
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
- 
+
 CHROMEDRIVER = "/usr/bin/chromedriver"
- 
+
+
 def get_driver(init_flg):
-     
-    #　ヘッドレスモードでブラウザを起動
+    # ヘッドレスモードでブラウザを起動
     options = Options()
     options.add_argument('--headless')
- 
- 
+
     # ブラウザーを起動
     driver = webdriver.Chrome(CHROMEDRIVER, options=options)
-     
+
     return driver
- 
+
+
 if __name__ == '__main__':
-     
+
     url = "https://example.com/"
-     
-    #　ヘッドレスモードでブラウザを起動
+
+    # ヘッドレスモードでブラウザを起動
     options = Options()
     options.add_argument('--headless')
- 
+
     # ブラウザーを起動
     driver = webdriver.Chrome(CHROMEDRIVER, options=options)
-     
+
     # urlにアクセス
     driver.get(url)
-     
+
     title = driver.find_element_by_tag_name("h1")
     print(title.text)
-     
+
     # ブラウザ停止
     driver.quit()
--- send_mail.py
+++ send_mail.py
@@ -4,33 +4,37 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def sendGMail(mail_subject:str='NULL subject',mail_message:str='NULL message'):
 
-	# SMTPサーバ接続
-	smtp_server='smtp.gmail.com'
-	port=587
-	server=smtplib.SMTP(smtp_server,port)
+def sendGMail(
+        mail_subject: str = 'NULL subject',
+        mail_message: str = 'NULL message'
+):
+    # SMTPサーバ接続
+    smtp_server = 'smtp.gmail.com'
+    port = 587
+    server = smtplib.SMTP(smtp_server, port)
 
-	# TLS暗号化設定
-	server.starttls()
+    # TLS暗号化設定
+    server.starttls()
 
-	# SMTPサーバログイン
-	login_address='techgate01sender@gmail.com'
-	login_password='ishstpmtmqcihare'
-	server.login(login_address,login_password)
+    # SMTPサーバログイン
+    login_address = 'techgate01sender@gmail.com'
+    login_password = 'ishstpmtmqcihare'
+    server.login(login_address, login_password)
 
-	# メッセージ作成・送信
-	message=MIMEMultipart()
-	message['Subject']=mail_subject
-	message['From']='techgate01sender@gmail.com'
-	message['To']='techno510tk@gmail.com'
+    # メッセージ作成・送信
+    message = MIMEMultipart()
+    message['Subject'] = mail_subject
+    message['From'] = 'techgate01sender@gmail.com'
+    message['To'] = 'techno510tk@gmail.com'
 
-	text=MIMEText(mail_message)
-	message.attach(text)
+    text = MIMEText(mail_message)
+    message.attach(text)
 
-	server.send_message(message)
+    server.send_message(message)
 
-	server.quit()
+    server.quit()
 
-if __name__=='__main__':
-	sendGMail()
+
+if __name__ == '__main__':
+    sendGMail()
